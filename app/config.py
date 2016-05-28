dc7c6dfcc4e168339958cbc6e524757207250800
atyrin <tyrin.andrew@gmail.com>
2016-05-28 23:49:58 +0300

    pep8, rm some files

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-
 NUMBER_OF_THREADS = 30
 BUMP_LIMIT = 300
-SPAN_CLASSES = ["spoiler","strong","italic","strikethrough","underline","highlight"] #For links see FormatLinks func. Syntax is [link][/link]
+SPAN_CLASSES = ["spoiler", "strong", "italic", "strikethrough", "underline",
+                "highlight"]  # For links see FormatLinks func. Syntax is [link][/link]
