27251fc377e066989c5458d1648a1395982cc6ea
atyrin <tyrin.andrew@gmail.com>
2018-03-02 01:55:59 +0100

    new thread api text formatting for input form

--- app/controller.py
+++ app/controller.py
@@ -6,8 +6,10 @@ from time import time
 
 import re
 from flask import request, redirect, render_template, make_response
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask.views import MethodView
+
+import werkzeug
 
 from app import minichan
 from app import model
@@ -17,6 +19,10 @@ from app import config
 from app import embeded_content
 
 api = Api(minichan)
+parser = reqparse.RequestParser()
+parser.add_argument('body')
+parser.add_argument('file', type=werkzeug.FileStorage, location='files')
+
 
 @minichan.route('/new', methods=['GET'])
 def newui():
@@ -26,22 +32,129 @@ def newui():
 class ThreadListAPI(MethodView):
     def get(self):
         return model.Thread.api, 200, {"X-Frame-Options": "ALLOW-FROM youtube.com"}
+
     def post(self):
-        pass
+        args = parser.parse_args()
+
+        print(args)
+
+        api_create_thread("temp subj", args["body"], args['file'])
+
+        return redirect("/new")
+
 
 class PostListAPI(MethodView):
     def get(self, thread_id):
         result = [dict(model.Thread.objects(post_id=thread_id).exclude('id')[0].to_mongo())]
-        result+=[dict(reply.to_mongo()) for reply in
-                           model.Reply.api(thread_link=model.Thread.objects(post_id=thread_id)[0])]
-        return result, 200, {"X-Frame-Options": "ALLOW-FROM youtube.com"} #for youtube support
+        result += [dict(reply.to_mongo()) for reply in
+                   model.Reply.api(thread_link=model.Thread.objects(post_id=thread_id)[0])]
+        return result, 200, {"X-Frame-Options": "ALLOW-FROM youtube.com"}  # for youtube support
 
     def post(self):
         pass
 
+
 api.add_resource(ThreadListAPI, '/api/threads')
 api.add_resource(PostListAPI, '/api/thread/<string:thread_id>')
 
+
+def api_create_thread(subject, body, file):
+    try:
+        thread = model.Thread()
+        thread.post_id = next_counter()
+        thread.body = body
+        thread.subject = subject
+        thread.bump_time = time()
+        thread.creation_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        thread.save()
+
+        api_upload_multimedia(file, thread)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+
+
+def api_upload_multimedia(file, post: model.Post):
+    try:
+        if file:
+            print("Filename = ", file.filename)
+            print("Content type = ", file.content_type)
+            print("mimetype = ", file.mimetype)
+            file_extension = file.filename.rsplit('.', 1)[-1]
+            print("type: " + file_extension)
+            post_attachment = model.Image(post_link=post)
+            if file_extension == "gif":
+                print("It's GIF")
+                post_attachment.img_src.put(file.stream, content_type='image/gif')
+            elif file_extension == "webm":
+                print("It's webm")
+                post_attachment.img_src.put(file.stream, content_type='video/webm')
+                thumbnail = thumbnail_generator.create_video_thumbnail(file.stream)
+                post_attachment.img_thumbnail_src.put(thumbnail, content_type='image/jpeg')
+            else:
+                print("Other extension: ", file_extension)
+                post_attachment.img_src.put(file.stream, content_type=file.mimetype)
+                thumbnail = thumbnail_generator.create_image_thumbnail(file.stream)
+                post_attachment.img_thumbnail_src.put(thumbnail, content_type='image/jpeg')
+
+                print("Other extension:", post_attachment.img_src.content_type)
+            print("Put done. Update thread")
+
+            post_attachment.img_id = str(hashlib.md5(post_attachment.img_src.read()).hexdigest())
+            post_attachment.save()
+            post.update(set__content_type=post_attachment.img_src.content_type)
+            post.update(set__image_id=post_attachment.img_id)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+
+
+@minichan.route('/img/<img_id>', methods=['GET'])
+def image(img_id):
+    image = model.Image.objects(img_id=img_id).first()
+    myimage = image.img_src.read()
+    response = make_response(myimage)
+    response.headers['Content-Type'] = 'image/jpeg'
+    return response
+
+
+@minichan.route('/thumb/<img_id>', methods=['GET'])
+def imagethumb(img_id):
+    image = model.Image.objects(img_id=img_id).first()
+    myimage = image.img_src.read()
+    response = make_response(myimage)
+    response.headers['Content-Type'] = 'image/jpeg'
+    return response
+
+
+@minichan.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
+
+
+def next_counter():
+    model.Counter.objects(name='post_counter').update_one(inc__next_id=1)
+    return model.Counter.objects[0].next_id
+
+
+def convert_text_to_span_class(text, class_name):
+    text = text.replace(("[" + class_name + "]"), "<span class=\"" + class_name + "\">")
+    text = text.replace(("[/" + class_name + "]"), "</span>")  # TO DO:Replace with regex
+    return text
+
+
+def format_links(text):
+    text = embeded_content.youtube_embed(text)
+    text = embeded_content.yandex_music(text)
+    return re.sub(r"\[link\](.*?)\[/link\]", r'<a target="_blank" href="\1">\1</a>', text)
+
+
+def format_text(text, span_classes):
+    for SpanClass in span_classes:
+        text = convert_text_to_span_class(text, SpanClass)
+    text = format_links(text)
+    return text
+
+
+# deprecated
 
 @minichan.route('/', methods=['GET', 'POST'])
 def board():
@@ -63,7 +176,6 @@ def board():
         return redirect('/')
     else:
         return render_template('board.html', data=model.Thread.all)
-
 
 
 @minichan.route('/<thread_id>', methods=['GET', 'POST'])
@@ -96,29 +208,6 @@ def thread(thread_id):
         response = make_response(render_template('thread.html', data=data))
         response.headers["X-Frame-Options"] = "ALLOW-FROM youtube.com"
         return response
-
-
-
-@minichan.route('/img/<img_id>', methods=['GET'])
-def image(img_id):
-    image = model.Image.objects(img_id=img_id).first()
-    myimage = image.img_src.read()
-    response = make_response(myimage)
-    response.headers['Content-Type'] = 'image/jpeg'
-    return response
-
-@minichan.route('/thumb/<img_id>', methods=['GET'])
-def imagethumb(img_id):
-    image = model.Image.objects(img_id=img_id).first()
-    myimage = image.img_src.read()
-    response = make_response(myimage)
-    response.headers['Content-Type'] = 'image/jpeg'
-    return response
-
-
-@minichan.errorhandler(500)
-def page_not_found(error):
-    return render_template('500.html'), 500
 
 
 def upload_multimedia(post_request, post: model.Post):
@@ -154,27 +243,3 @@ def upload_multimedia(post_request, post: model.Post):
             post.update(set__image_id=post_attachment.img_id)
     except:
         print("Unexpected error:", sys.exc_info()[0])
-
-
-def next_counter():
-    model.Counter.objects(name='post_counter').update_one(inc__next_id=1)
-    return model.Counter.objects[0].next_id
-
-
-def convert_text_to_span_class(text, class_name):
-    text = text.replace(("[" + class_name + "]"), "<span class=\"" + class_name + "\">")
-    text = text.replace(("[/" + class_name + "]"), "</span>")  # TO DO:Replace with regex
-    return text
-
-
-def format_links(text):
-    text = embeded_content.youtube_embed(text)
-    text = embeded_content.yandex_music(text)
-    return re.sub(r"\[link\](.*?)\[/link\]", r'<a target="_blank" href="\1">\1</a>', text)
-
-
-def format_text(text, span_classes):
-    for SpanClass in span_classes:
-        text = convert_text_to_span_class(text, SpanClass)
-    text = format_links(text)
-    return text
--- ui/src/InputForm.js
+++ ui/src/InputForm.js
@@ -1,9 +1,51 @@
 import React, {Component} from 'react';
-import {Panel, PanelGroup} from 'react-bootstrap';
+import {
+    Panel,
+    PanelGroup,
+    FormGroup,
+    ControlLabel,
+    FormControl,
+    Button,
+    ButtonGroup,
+    DropdownButton,
+    MenuItem
+} from 'react-bootstrap';
 
 class InputForm extends Component {
     constructor(props) {
         super(props);
+        this.validateSubmit = this.validateSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.handleBoldButton = this.handleBoldButton.bind(this);
+        this.handleItalicButton = this.handleItalicButton.bind(this);
+        this.handleStyleButton = this._handleStyleButton.bind(this);
+        this.state = {text: ""};
+    }
+
+    validateSubmit(e) {
+        console.log(e);
+    }
+
+    _handleStyleButton(type1, type2) {
+        let textVal = this.refs.myTextarea;
+        let selectionStart = textVal.selectionStart;
+        let selectionEnd = textVal.selectionEnd;
+        var newVal = this.state.text.substring(0, selectionStart) + type1 +
+            this.state.text.substring(selectionStart, selectionEnd) + type2 +
+            this.state.text.substring(selectionEnd);
+        this.setState({text: newVal})
+    }
+
+    handleBoldButton() {
+        this._handleStyleButton("[b]", "[/b]")
+    }
+
+    handleItalicButton() {
+        this._handleStyleButton("[i]", "[/i]")
+    }
+
+    handleTextChange(e) {
+        this.setState({text: e.target.value});
     }
 
     render() {
@@ -15,8 +57,20 @@ class InputForm extends Component {
             },
             header: {
                 textAlign: "center",
+            },
+            submitStyle: {
+                float: "right"
+            },
+            textAreaStyle: {
+                float: "left",
+                width: "100%",
+                height: "120px",
+                border: "3px solid #cccccc",
+                padding: "5px",
+                fontFamily: "Tahoma, sans-serif",
+                marginBottom: "5px",
             }
-        }
+        };
         return (
             <Panel style={styles.all} id="collapsible-panel-example-2">
                 <Panel.Heading>
@@ -26,8 +80,39 @@ class InputForm extends Component {
                 </Panel.Heading>
                 <Panel.Collapse>
                     <Panel.Body>
-                        Тут будет поле для ввода, кнопочки форматирования, возможность залить файл или приаттачить видос
-                        с ютуба
+                        <form action="/api/threads" method="post" enctype="multipart/form-data">
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>Ваше сообщение</ControlLabel>
+                                <textarea
+                                    type="text"
+                                    name="body"
+                                    style={styles.textAreaStyle}
+                                    placeholder="Введите свой текст"
+                                    onChange={this.handleTextChange}
+                                    value={this.state.text}
+                                    ref="myTextarea"/>
+                            </FormGroup>
+                            <ButtonGroup>
+                                <Button onClick={this.handleBoldButton}>Bold</Button>
+                                <Button onClick={this.handleItalicButton}>Italic</Button>
+                                <DropdownButton title="Attach" id="bg-nested-dropdown">
+                                    <MenuItem eventKey="1">Image</MenuItem>
+                                    <MenuItem eventKey="2">Webm</MenuItem>
+                                </DropdownButton>
+                            </ButtonGroup>
+                            <input type="file"
+                                   name="file"
+                                   accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|images/*, .webm"
+                                   onChange={(e) => this.validateSubmit(e.target.files)}/>
+                            <Button
+                                bsStyle="success"
+                                type="submit"
+                                style={styles.submitStyle}
+                                onClick={this.validateSubmit}
+                            >
+                                Submit
+                            </Button>
+                        </form>
                     </Panel.Body>
                 </Panel.Collapse>
             </Panel>
