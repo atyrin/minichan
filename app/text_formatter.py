eb0bb70fc9a44c3b6398174d3eece691911ba484
atyrin <tyrin.andrew@gmail.com>
2018-03-03 17:54:57 +0100

    regexp for text formatting reply parser

--- app/text_formatter.py
+++ app/text_formatter.py
@@ -2,17 +2,27 @@ import re
 
 
 def convert_text_to_span_class(text, class_name):
-    text = text.replace(("[" + class_name + "]"), "<span class=\"" + class_name + "\">")
-    text = text.replace(("[/" + class_name + "]"), "</span>")  # TO DO:Replace with regex
-    return text
+    return re.sub(r"\[" + class_name + "\](.*)\[/" + class_name + "\]",
+                  lambda m: "<span class=\"" + class_name + "\">{0}</span>".format(m.group(1)),
+                  text)
 
 
 def format_links(text):
-    return re.sub(r"\[link\](.*)\[/link\]", r'<a href="\1">\1</a>', text)
+    return re.sub(r"\[link\](.*)\[/link\]",
+                  lambda m: r'<a href="{0}">{0}</a>'.format(
+                      m.group(1) if re.search('http', m.group(1)) else "http://" + m.group(1)),
+                  text)
+
+
+def format_reply(text):
+    return re.sub(">>(\d*)",
+                  lambda m: '<span class=\"reply\" data-target=\"{1}\">{0}</span>'.format(m.group(0), m.group(1)),
+                  text)
 
 
 def format_text(text, span_classes):
     for SpanClass in span_classes:
         text = convert_text_to_span_class(text, SpanClass)
     text = format_links(text)
+    text = format_reply(text)
     return text
